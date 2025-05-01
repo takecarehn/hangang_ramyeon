1136be0f7287dbbf166f7b9e8d319ff0be431309
NOBITA-PC\dell_ <nobi.nobita.kid@gmail.com>
2025-05-01 11:05:40 +0700

    Add ProductCategories api

--- src/Infrastructure/Identity/IdentityService.cs
+++ src/Infrastructure/Identity/IdentityService.cs
@@ -87,6 +87,7 @@ public class IdentityService : IIdentityService
 
     public async Task<IList<string>> GetUserPermissionsAsync(string userId)
     {
+        _cache.Remove($"permissions_{userId}");
         return await _cache.GetOrCreateAsync($"permissions_{userId}", async entry =>
         {
             entry.AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(30);
